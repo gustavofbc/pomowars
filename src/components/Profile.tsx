e3319f585ea284a58a2d5afc6a3eac1e00aa1e73
Gustavo Felipe Batista Carneiro <gustavo.felipebc@gmail.com>
2021-02-25 20:52:47 -0300

    implements function completeChallenge

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Profile.module.css";
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/gustavofbc.png" alt="Gustavo Felipe" />
@@ -8,7 +11,7 @@ export function Profile() {
         <strong>Gustavo Felipe</strong>
         <p>
           <img src="icons/level.svg" alt="level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
-import { Profile } from "../components/Perfil";
+import { Profile } from "../components/Profile";
 import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from "../styles/pages/home.module.css";
